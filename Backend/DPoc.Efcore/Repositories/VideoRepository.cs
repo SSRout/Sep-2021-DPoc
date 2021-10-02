4a3256692d6bfa64879d03cc7a6c4d7c6ad6bb16
ssrout <smruti.sambit.rout@gmail.com>
2021-10-02 22:04:07 +0530

    added relation using fluent api

--- Backend/DPoc.Efcore/Repositories/VideoRepository.cs
+++ Backend/DPoc.Efcore/Repositories/VideoRepository.cs
@@ -10,7 +10,7 @@ namespace DPoc.Efcore.Repositories
 {
     public class VideoRepository : IVideoRepository
     {
-        private VideoApplicationDbContext _ctx;
+        private readonly VideoApplicationDbContext _ctx;
 
         public VideoRepository(VideoApplicationDbContext ctx)
         {
@@ -18,27 +18,80 @@ namespace DPoc.Efcore.Repositories
         }
         public Video Add(Video video)
         {
-            throw new NotImplementedException();
+            var entity = _ctx.Videos.Add(new Entities.VideoEntity
+            {
+                Id=video.Id,
+                Title = video.Title,
+                ReleaseDate = video.ReleaseDate,
+                StoryLine = video.StoryLine
+            }).Entity;
+            _ctx.SaveChanges();
+            return new Video()
+            {
+                Id = entity.Id,
+                Title = entity.Title,
+                ReleaseDate = entity.ReleaseDate,
+                StoryLine = entity.StoryLine,
+                Genre = new Genre
+                {
+                    Id = entity.Genre.Id
+                }
+            };
         }
 
         public Video Delete(int id)
         {
-            throw new NotImplementedException();
+            var res = _ctx.Videos.Remove(new Entities.VideoEntity { Id = id }).Entity;
+            _ctx.SaveChanges();
+            return new Video { Id = res.Id };
         }
 
         public List<Video> FindAll()
         {
-            throw new NotImplementedException();
+            return _ctx.Videos.Select(videoEntity => new Video { Id = videoEntity.Id, Title = videoEntity.Title,ReleaseDate=videoEntity.ReleaseDate,StoryLine=videoEntity.StoryLine,
+                Genre = new Genre
+                {
+                    Id = videoEntity.Genre.Id,
+                    Name = videoEntity.Genre.Name
+                }
+            }).ToList();
         }
 
         public Video FindVideoById(int id)
         {
-            throw new NotImplementedException();
+            //var res = _ctx.Videos.FirstOrDefault(video => video.Id == id);
+            //if (res != null)
+            //    return new Video { Id = res.Id, Title = res.Title, ReleaseDate = res.ReleaseDate, StoryLine = res.StoryLine };
+            //return null;
+
+            return _ctx.Videos.Select(entity=>new Video() {
+                Id = entity.Id,
+                Title = entity.Title,
+                ReleaseDate = entity.ReleaseDate,
+                StoryLine = entity.StoryLine,
+                Genre = new Genre
+                {
+                    Id = entity.Genre.Id,
+                    Name=entity.Genre.Name
+                }
+            }).FirstOrDefault(video => video.Id == id);
         }
 
         public Video UpdateVideo(Video video)
         {
-            throw new NotImplementedException();
+            var entity = _ctx.Videos.Update(new Entities.VideoEntity { Id =video.Id, Title = video.Title,ReleaseDate=video.ReleaseDate,StoryLine=video.StoryLine,GenreId=video.Genre.Id }).Entity;
+            _ctx.SaveChanges();
+            return new Video
+            {
+                Id = entity.Id,
+                Title = entity.Title,
+                ReleaseDate = entity.ReleaseDate,
+                StoryLine = entity.StoryLine,
+                Genre = new Genre
+                {
+                    Id = entity.Genre.Id
+                }
+            };
         }
     }
 }
