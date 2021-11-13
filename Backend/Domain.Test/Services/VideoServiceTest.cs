e063f360d333edbb15fa0675d3d1714d900c8879
Smruti Sambit Rout <smruti.sambit.rout@gmail.com>
2021-11-13 12:19:34 +0530

    videoservice test (#5)

--- Backend/Domain.Test/Services/VideoServiceTest.cs
+++ Backend/Domain.Test/Services/VideoServiceTest.cs
@@ -1,8 +1,10 @@
 using InnoTech.VideoApplication2021.Domain.IRepositories;
 using InnoTech.VideoApplication2021.Domain.Services;
 using InnotTech.VideoApplication2021.Core.IServices;
+using InnotTech.VideoApplication2021.Core.Models;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Xunit;
 
@@ -10,19 +12,45 @@ namespace Domain.Test.Services
 {
     public class VideoServiceTest
     {
+        private readonly Mock<IVideoRepository> _mock;
+        private readonly VideoService _service;
+
+        public VideoServiceTest()
+        {
+            _mock= new Mock<IVideoRepository>();
+            _service= new VideoService(_mock.Object);
+        }
+
         [Fact]
         public void VideoService_IsIVideoService()
         {
-            var mock = new Mock<IVideoRepository>();
-            var service = new VideoService(mock.Object);
-            Assert.True(service is IVideoService);
+            Assert.True(_service is IVideoService);
         }
 
         [Fact]
         public void VideoService_WithNullvideoRepository_ThrowInvalidDataException()
         {
             Assert.Throws<InvalidDataException>(()=>new VideoService(null));
+        }
 
+        [Fact]
+        public void GetVideos_CallVideoRepositoryFindAll_ExactlyOnce()
+        {
+            _service.ReadAll();
+            _mock.Verify(r => r.FindAll(), Times.Once);
+        }
+
+        [Fact]
+        public void GetVideos_NoFilter_ReturnListOfAllVideos()
+        {
+            var expected = new List<Video>
+            {
+                new Video{Id=1,Genre=null,ReleaseDate=DateTime.Now,Title="test1",StoryLine=""},
+                new Video{Id=2,Genre=null,ReleaseDate=DateTime.Now,Title="test2",StoryLine=""}
+            };
+            _mock.Setup(r => r.FindAll()).Returns(expected);
+            var actual = _service.ReadAll();
+            Assert.Equal(expected, actual);
         }
     }
 }
